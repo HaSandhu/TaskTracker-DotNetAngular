a406bdf8e172fe15399dd1e6272fa94453f88faa
hasandhu <sandhuenzo@gmail.com>
2025-07-12 18:14:34 -0500

    Task Form

--- frontend/task-tracker-angular/src/app/components/task-form/task-form.ts
+++ frontend/task-tracker-angular/src/app/components/task-form/task-form.ts
@@ -12,20 +12,23 @@ import { FormsModule } from '@angular/forms';
 })
 export class TaskForm {
   createdTask? : Task;
-
   //Task Variables
   taskTitle! : string;
   taskDescription! : string;
-  taskStatus! :  0 | 1 | 2 | 3 | 4;
   taskPriority? : 'high' | 'medium' | 'low';
   taskDueDate? : string;
-
+  tags: string[] = [];
+  tag?: string;
+  // total: number = this.tags?.length
   wasCreated: boolean = false;
 
 
   constructor(private service : TaskService) {}
 
-
+  submitTag(){
+    if (this.tag !== null && this.tag !== undefined)
+      this.tags.push(this.tag);
+  }
 
 
   createTask() {
@@ -33,22 +36,22 @@ export class TaskForm {
       id: 0,
       title: this.taskTitle,
       description: this.taskDescription,
-      status: this.taskStatus,
+      status: 0,
       taskPriority: this.taskPriority,
       dueDate: this.taskDueDate
     };
-
+    console.log(this.createTask);
     if (this.createdTask==null) return;
 
 
-    this.service.createTask(this.createdTask).subscribe({
-      next: (res: any) => {
-        console.log("Res Status: " + res);
-        // console.div(res);
-        if (res.status == 204)
-          this.wasCreated = true
-      }
-    })
+    // this.service.createTask(this.createdTask).subscribe({
+    //   next: (res: any) => {
+    //     console.log("Res Status: " + res);
+    //     // console.div(res);
+    //     if (res.status == 204)
+    //       this.wasCreated = true
+    //   }
+    // })
 
 
   }
