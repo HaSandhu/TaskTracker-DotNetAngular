52c2e35be67efbc69107a06a76ac3df1159285cc
hasandhu <sandhuenzo@gmail.com>
2025-07-13 14:15:18 -0500

    Commiting internal files just in case I need these to run this later on

--- backend/TaskTracker.API/obj/Debug/net9.0/TaskTracker.API.AssemblyInfo.cs
+++ backend/TaskTracker.API/obj/Debug/net9.0/TaskTracker.API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TaskTracker.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f52c836117df6727aca46924b8e95b4a6099517f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+45041cf81b0f629e33522e5522832f5dc4a55543")]
 [assembly: System.Reflection.AssemblyProductAttribute("TaskTracker.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TaskTracker.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
